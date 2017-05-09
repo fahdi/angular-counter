8220d6bda82d630fd2c15af8bf87311a94cf6757
Fahad Murtaza <fahdim@gmail.com>
2017-05-09 18:51:56 +0500

    Adding styles to the counter component

--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -13,7 +13,27 @@ import { Component, OnInit } from '@angular/core';
       </button>
     </div>
   `,
-  styles: []
+  styles: [`
+    .counter {
+      position: relative;
+    }
+    input {
+      border: 0;
+      border-radius: 3px;
+      height: 30px;
+      max-width: 100px;
+      text-align: center;
+    }
+    button {
+      outline: 0;
+      cursor: pointer;
+      height: 30px;
+      border: 0;
+      border-radius: 3px;
+      background: #0088cc;
+      color: #fff;
+    }
+  `],
 })
 export class CounterComponent implements OnInit {
 
