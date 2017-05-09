2971e587f080aa63c4ef5f5919927b7543dfe712
Fahad Murtaza <fahdim@gmail.com>
2017-05-09 19:26:15 +0500

    Adding the ability to initialize the count variable in the parent component and then using Input() in the counter component to specify that it is an input from the parent component

--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-counter',
@@ -37,7 +37,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CounterComponent implements OnInit {
 
-  count: number = 0;  
+  @Input() count: number = 0;  
 
   ngOnInit() {
   }
